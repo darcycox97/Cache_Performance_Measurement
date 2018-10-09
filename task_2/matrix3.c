27d57d121d65838068c388063c2affe5738e4828
Darcy Cox <darcycox97@gmail.com>
2018-10-09 13:36:48 +1300

    Add matrix3 which is a blocking implementation of matrix multiplication. Way faster

--- task_2/matrix3.c
+++ task_2/matrix3.c
@@ -4,82 +4,133 @@
 #include <sys/time.h>
 #include <time.h>
 
-double getTime(){
+double getTime()
+{
   struct timeval t;
   double sec, msec;
-  
+
   while (gettimeofday(&t, NULL) != 0);
   sec = t.tv_sec;
   msec = t.tv_usec;
-  
-  sec = sec + msec/1000000.0;
-  
+
+  sec = sec + msec / 1000000.0;
+
   return sec;
 }
- 
-/* for task 1 only */
-void usage(void)
-{
-	fprintf(stderr, "Usage: cachetest1/2 [--repetitions M] [--array_size N]\n");
-	exit(1);
-}
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  double t1, t2; 
-  
-  /* variables for task 1 */
-  unsigned int M = 1000;
-  unsigned int N = 256*1024; 
+  double t1, t2;
+
+  /* Declare variables */
+  int N = 1000;
   unsigned int i;
-	
-  /* declare variables; examples, adjust for task */
-	//int *a;
-	double  a[100];
- 
-  
-  /* parameter parsing task 1 */
-  for(i=1; i<(unsigned)argc; i++) {
-	  if (strcmp(argv[i], "--repetitions") == 0) {
-		  i++;
-		  if (i < argc)
-			  sscanf(argv[i], "%u", &M);
-		  else
-			  usage();
-	  } else if (strcmp(argv[i], "--array_size") == 0) {
-		  i++;
-		  if (i < argc)
-			  sscanf(argv[i], "%u", &N);
-		  else
-			  usage();
-	  } else usage();
+  unsigned int j;
+  unsigned int k;
+  unsigned int blockSize; // block size is blockSize x blockSize
+  unsigned int rowOffset;
+  unsigned int colOffset;
+  double sum;
+
+  // we will compute A * B and store it in C
+  double **A;
+  double **B;
+  double **C;
+
+  /* Allocate memory for the matrices */
+  A = malloc(N * sizeof(double *));
+  B = malloc(N * sizeof(double *));
+  C = malloc(N * sizeof(double *));
+  for (i = 0; i < N; i++)
+  {
+    A[i] = malloc(N * sizeof(double));
+    B[i] = malloc(N * sizeof(double));
+    C[i] = malloc(N * sizeof(double));
   }
 
-    
-  /* allocate memory for arrays; examples, adjust for task */
-	 //a = malloc (N * sizeof(int));
+  /* zero the matrices */
+  for (i = 0; i < N; i++)
+  {
+    for (j = 0; j < N; j++)
+    {
 
-	 /* initialise arrray elements */
- 
-	 
+      A[i][j] = 0;
+      B[i][j] = 0;
+      C[i][j] = 0;
+    }
+  }
+
+  /* Put some values into the operand matrices so we know the multiplication is correct. 
+	 * With the values, C[0][0] = 26, C[0][1] = 46, C[1][0] = 22, C[1][1] = 39 */
+	A[0][0] = 1;
+	A[0][1] = 2;
+	A[0][2] = 3;
+	A[0][3] = 4;
+	A[1][0] = 2;
+	A[1][1] = 1;
+	A[1][2] = 2;
+	A[1][3] = 3;
+
+	B[0][0] = 3;
+	B[0][1] = 5;
+	B[1][0] = 2;
+	B[1][1] = 1;
+	B[2][0] = 1;
+	B[2][1] = 5;
+	B[3][0] = 4;
+	B[3][1] = 6;
+
   t1 = getTime();
   /* code to be measured goes here */
   /***************************************/
-	
-	
-	
-	
+
+  // compute A * B using blocking and store it in C
+  // A is partitioned into slivers of length blockSize, and B is partitioned into blocks of blockSize x blockSize. 
+  // For each blockSize x blockSize block of B, N 1 x blockSize slivers are accessed. The sum of each entry in C
+  // is accumulated over several blocks before the entry is fully computed.
+  blockSize = 64; //TODO get best block size (relate to cache line) and blockSize should divide evenly into N(?)
+  for (rowOffset = 0; rowOffset < N; rowOffset += blockSize)
+  {
+    for (colOffset = 0; colOffset < N; colOffset += blockSize)
+    {
+      // for each block, calculate the part of each entry in C
+      // that we possibly can while only using the blockSize x blockSize block
+      for (i = 0; i < N; i++)
+      {
+        for (j = colOffset; j < colOffset + blockSize && j < N; j++)
+        {
+          // adding onto the previously accumulated value for this particular C entry
+          sum = C[i][j];
+          for (k = rowOffset; k < rowOffset + blockSize && k < N; k++)
+          {
+            sum += A[i][k] * B[k][j];
+          }
+          // further accumulate the value for this entry
+          C[i][j] = sum;
+        }
+      }
+    }
+  }
   /***************************************/
-	t2 = getTime(); 
-  
-  /* output; examples, adjust for task */
-  printf("time: %6.2f secs\n",(t2 - t1));
+  t2 = getTime();
 
-  /* IMPORTANT: also print the result of the code, e.g. the sum, 
-   * otherwise compiler might optimise away the code */
+  printf("time: %6.2f secs\n", (t2 - t1));
+  printf("C[0][0]: %.1lf\n", C[0][0]);
+	printf("C[0][1]: %.1lf\n", C[0][1]);
+	printf("C[1][0]: %.1lf\n", C[1][0]);
+	printf("C[1][1]: %.1lf\n", C[1][1]);
   
-  /* free memory; examples, adjust for task */
-  //free(a);
+  /* Free memory allocated to arrays */
+  for (i = 0; i < N; i++)
+  {
+    free(A[i]);
+    free(B[i]);
+    free(C[i]);
+  }
 
-  return 0;  
+  free(A);
+  free(B);
+  free(C);
+
+  return 0;
 }
